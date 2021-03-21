576dcf6869db4457e5b73ed5c932293552779056
kakash <kakash@10.0.0.164>
2021-03-21 14:39:28 -0500

    anything method

--- src/demo2/GithubDemoTwo.java
+++ src/demo2/GithubDemoTwo.java
@@ -5,8 +5,12 @@ public class GithubDemoTwo {
 	public static void main(String[] args) {
 		System.out.println("My first github commit....");
 		System.out.println("this is me");
-
+		
 	}
 	//you added new code and now you have to share with the team
+	public void anything() {
+	System.out.println("****stars****");
+
+	}
 
 }
